e308f58f55a2df4aa86252f3e546b0c1be5d8c14
cerkvenihaxel <cerkvenihaxel@gmail.com>
2022-05-18 15:40:48 -0300

    Pricing to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import '../ components/Home'
+import '../ components/Home';
+import '../pricing/Pricing';
 function App() {
   return (
     <div className="App">
       
     <Home/> 
-    
+
 
 
 
