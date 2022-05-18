bda955407b4bd31bdc08ab070b068927b7556e4f
cerkvenihaxel <cerkvenihaxel@gmail.com>
2022-05-18 18:01:44 -0300

    NavBar funciona genial

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,13 @@
 import React, {Component} from "react";
-import Pricing from './Pricing';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import Typography from '@mui/material/Typography';
+import ResponsiveAppBar from "./Navbar";
 
 
 export default class Home extends Component{
 
 render(){
     return(
-     <Typography variant="h1" component="h2"> 
-     Cerro Global LR
-     </Typography>
+    <ResponsiveAppBar/>
+    
      );
     }
 }
