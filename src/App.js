35b08a028ab1664667e1a6b692bc3fc0561421fd
cerkvenihaxel <cerkvenihaxel@gmail.com>
2022-05-18 16:03:42 -0300

    Mui working ok Typgraphy

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import '../ components/Home';
-import '../pricing/Pricing';
+import Home from './components/Home'
+
+
 function App() {
   return (
     <div className="App">
       
-    <Home/> 
-
+    <Home/>;
+    
 
 
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,3 @@
+import React from "react";
+import Pricing from "../pricing/Pricing";
+
